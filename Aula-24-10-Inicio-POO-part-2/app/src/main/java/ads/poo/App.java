dfae6cf67426c5ec9affc83b54016ac88ce3d54e
jaozinh0o <joao.gg2004@aluno.ifsc.edu.br>
2024-10-27 17:55:36 -0300

    Buzzlightyear finalizado

--- Aula-24-10-Inicio-POO-part-2/app/src/main/java/ads/poo/App.java
+++ Aula-24-10-Inicio-POO-part-2/app/src/main/java/ads/poo/App.java
@@ -6,8 +6,13 @@ public class App {
 
         BuzzLightyear b = new BuzzLightyear();
 
+        System.out.println(b.capacete());
         System.out.println(b.asa());
+        System.out.println(b.golpeKarate());
+        System.out.println(b.falar());
         System.out.println(b.asa());
+        System.out.println(b.laser());
+        System.out.println(b.capacete());
 
     }
 }
--- Aula-24-10-Inicio-POO-part-2/app/src/main/java/ads/poo/BuzzLightyear.java
+++ Aula-24-10-Inicio-POO-part-2/app/src/main/java/ads/poo/BuzzLightyear.java
@@ -3,28 +3,54 @@ package ads.poo;
 public class BuzzLightyear {
 
     // Atributos:
-    private boolean asa;
-    private boolean capacete;
-    private boolean laser;
+    private boolean asa = false;
+    private boolean capacete = true;
     private String[] frase = {"Ao infinito e além!", "Preparado para voar!", "Comandao estelar pronto para voar!",
             "Consigo resolver todas as missões do universo.", "Saudações, eu sou Buzz Lightyear e venho em missão de paz"};
 
     // Métodos:
-
     
+    // Asa:
 
     public String asa(){
-        if (asa = true){
-            this.asa = false;
+        if (asa == true){
+            asa = false;
             return "Fechando asas!";
         }else {
-            return "Preparador para voar!";
+            asa = true;
+            return "Abrindo asas!";
         }
     }
 
+    // Frase:
 
+    public String falar(){
+        int i = (int) (Math.random() * frase.length);
+        return frase[i]; 
+    }
 
+    // Capacete
 
+    public String capacete(){
+        if (capacete == true){
+            capacete = false;
+            return "Abrindo capacete!";
+        }else {
+            capacete = true;
+            return "Fechando capacete!";
+        }
+    }
 
+    // Laser:
+
+    public String laser(){
+        return "Disparando laser *ZZZUM*";
+    }
+
+    // Soco
+
+    public String golpeKarate(){
+        return "Golpe de karatê estelar!";
+    }
 
 }
