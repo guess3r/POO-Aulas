64281a3a1bc1e26e7c87e4bf99d569dbed3a182f
guesser <joao.gg2004@aluno.ifsc.edu.br>
2024-12-19 19:33:07 -0300

    Commit emergencia

---
+++ Aula16-19-12/app/src/main/java/ads/poo/produtos/Telefone.java
@@ -0,0 +1,29 @@
+package ads.poo.produtos;
+
+public class Telefone {
+
+    private int codigo;
+    private String numSerie;
+    private String modelo;
+    private double peso;
+    private Dimensao dimensao;
+
+    public Telefone(int codigo, String numSerie, String modelo, double peso, Dimensao dimensao) {
+        this.codigo = codigo;
+        this.numSerie = numSerie;
+        this.modelo = modelo;
+        this.peso = peso;
+        this.dimensao = dimensao;
+    }
+
+    public String imprimirDados() {
+        return "Telefone{" +
+                "codigo=" + codigo +
+                ", numSerie='" + numSerie + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", peso=" + peso +
+                ", dimensao=" + dimensao +
+                '}';
+    }
+
+}
